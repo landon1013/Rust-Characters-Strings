ae49daf27b6b3be0e3a83e6365a73a82026df95d
Landon Call <landoncall@Landons-MacBook-Pro.local>
2020-11-30 13:27:03 -0700

    Strings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+// Strings
+fn strings() {
+    let s:&'static str = "Hello there!";
+
+    for c in s.chars().rev() { //Can be manipulated like any other array
+        println!("{}", c);
+    }
+
+    if let Some(first_char) = s.chars().nth(0) {
+        println!("First letter is {}", first_char);
+    }
+
+    let mut letters = String::new();
+    let mut a = 'a' as u8;
+    while a <= ('z' as u8) {
+        letters.push(a as char);
+        letters.push_str(",");
+        a += 1;
+    }
+
+    println!("{}", letters);
+}
+
 fn main() {
-    println!("Hello, world!");
+    strings();
 }
